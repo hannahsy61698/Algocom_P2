35348cd585b41a8a59c8de2755c0d06294d3af21
Angel <angelica_limkico@dlsu.edu.ph>
2019-07-02 22:40:04 +0800

    update

--- src/algocom_p2/Algocom_P2.java
+++ src/algocom_p2/Algocom_P2.java
@@ -1,21 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package algocom_p2;
 
-/**
- *
- * @author Hannah Sy
- */
 public class Algocom_P2 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    
+    public static void checkHarvest (int numVillages, int costToFeed, int[] harvests) {
+	long start = System.currentTimeMillis();
+		
+	int total = numVillages * costToFeed;
+	int food = 0;
+		   
+	for(int i = 0; i < harvests.length; i++) {
+            food = food  + harvests[i];
+	}
+		
+	System.out.println(total + " " + food);
+	if(food == total)
+            System.out.println("JUST ENOUGH FOR EVERYONE");
+	else if(food > total)
+                System.out.println("PARTY!");
+            else
+		System.out.println("NOT ENOUGH FOOD");
+		   
+	long end = System.currentTimeMillis();
+		
+	double sec = end- start;
+		
+	System.out.println(start + " - " + end + " = " + sec);
     }
     
+    public static void main(String[] args) {
+        
+        //#3 of the assignment
+        int[] har = new int[10000000];
+        for(int i = 0; i < 10000000; i++) {
+            har[i] = 1000;
+	}
+		
+	checkHarvest(100, 500, har);
+    }
 }
